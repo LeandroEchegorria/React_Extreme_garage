3a381c7d1cd90f4169a60743fd4095c40ec90c96
Leandro Echegorría <lechegorria@teco.com.ar>
2022-12-02 19:00:54 -0300

    Services actualization

--- src/pages/Servicios.js
+++ src/pages/Servicios.js
@@ -9,21 +9,39 @@ const Servicios = () => {
       <div className='servicios'>
         <h2>NUESTROS SERVICIOS</h2>
         <div>
-          <p className='items1' id='items'>Tratamiento acrílico y cerámico</p>
-          <p className='items2' id='items'>Venta y colocación de CreeLED</p>
-          <p className='items3' id='items'>Mecánica ligera</p>
-          <p className='items4' id='items'>Ploteo vehicular</p>
-          <p className='items5' id='items'>Polarizado</p>
-          <p className='items6' id='items'>Lavado de motor</p>
-          <p className='items7' id='items'>Service automotor (Aceite, filtros, caja)</p>
-          <p className='items8' id='items'>y mucho más....</p>
+          <div>
+            <h2 className='items1' id='items'>Detailing:</h2>
+            <p className='items2' id='items'>Tratamiento acrílico y cerámico</p>
+            <p className='items2' id='items'>Vidrio Líquido</p>
+            <p className='items2' id='items'>Pulido de ópticas</p>
+            <p className='items2' id='items'>Limpieza de interior (Tapizado)</p>
+            <p className='items2' id='items'>Lavado de motor</p>
+          </div>
+          <div>
+            <h2 className='items3' id='items'>Autoradio:</h2>
+            <p className='items4' id='items'>Ploteo vehicular</p>
+            <p className='items4' id='items'>Polarizado</p>
+            <p className='items4' id='items'>Blackhousing</p>
+            <p className='items4' id='items'>CreeLED</p>
+            <p className='items4' id='items'>Tira DRL</p>
+            <p className='items4' id='items'>Giro Fijo</p>
+            <p className='items4' id='items'>Música</p>
+            <p className='items4' id='items'>Carga de Aire Acondicionado</p>
+            <p className='items4' id='items'>Cierre centralizado</p>
+            <p className='items4' id='items'>Levanta cristales</p>
+            <p className='items4' id='items'>Alarma</p>
+          </div>
+          <div>
+            <h2 className='items5' id='items'>Mecánica:</h2>
+            <p className='items6' id='items'>Tren Delantero</p>
+            <p className='items6' id='items'>Suspensión</p>
+            <p className='items6' id='items'>Frenos</p>
+          </div>
         </div>
       </div>
     </Container>
-    
-    
   )
 }
 
-export default Servicios
+export default Servicios;
 
